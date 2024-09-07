791daaa5a4029aa23b8850bef5edaf2c1f8911fa
Kyoung Yeob <kkyub1313@gmail.com>
2024-09-07 18:09:15 +0900

    auth 컨트롤러, 서비스 init

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,58 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Head, Headers, Post, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
 
 @Controller('auth')
 export class AuthController {
-  constructor(private readonly authService: AuthService) {}
+  constructor(private readonly authService: AuthService) { }
+
+  @Post('token/access')
+  postTokenAccess(@Headers('authorization') rawToken: string) {
+    const token = this.authService.extractTokenFromHeader(rawToken, true);
+    const newToken = this.authService.rotateToken(token, false);
+
+    return {
+      accessToken: newToken
+    }
+  }
+
+  @Post('token/refresh')
+  postTokenRefresh(@Headers('authorization') rawToken: string) {
+    const token = this.authService.extractTokenFromHeader(rawToken, true);
+
+    const newToken = this.authService.rotateToken(token, true);
+
+    return {
+      refreshToken: newToken,
+    }
+  }
+
+
+  @Post('login/email')
+  postLoginEmail(
+    @Headers('authorization') rawToken: string,
+  ) {
+    // email:password -> base64
+    // acncsjklded -> email:password
+    const token = this.authService.extractTokenFromHeader(rawToken, false);
+
+    const credentials = this.authService.decodeBasicToken(token);
+    console.log('Decoded credentials:', credentials.email);
+
+
+    return this.authService.loginWithEmail(credentials);
+  }
+
+  @Post('register/email')
+  postRegisterEmail(@Body() body: RegisterUserDto) {
+    return this.authService.registerWithEmail(body);
+  }
+
+
+
+
+
+
+
+
+
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,103 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { JwtService } from '@nestjs/jwt';
+import { ENV_JWT_SECRET_KEY } from 'src/common/const/env-keys.const';
+import { UsersService } from 'src/users/users.service';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+    constructor(
+        private readonly jwtService: JwtService,
+        private readonly usersService: UsersService,
+        private readonly configService: ConfigService,
+    ) { }
+
+    /**
+     * 토큰을 사용하게 되는 방식
+     * 
+     * 1) 사용자가 로그인 or 회원가입 진행시
+     *      accessToken 과 refreshToken 을 발급받는다.
+     * 2) 로그인 할때는 Basic 토큰과 함께 요청을 보낸다.
+     *      Basic 토큰은 '이메일:비밀번호'를 Base64로 인코딩한 형태이다.
+     *      예) {authorization: 'Basic {toekn}'}
+     * 3) 아무나 접근할 수 없는 정보(private route)를 접근할 때는 
+     *      accessToken을 Header 에 추가해서 요청과 함께 보낸다.
+     *      예) {authorization: 'Bearer {token}'}
+     * 4) 토큰과 요청을 함께 받은 서버는 토큰 검증을 통해 현재 요청을 보낸
+     *      사용자가 누군지 알 수 있다.
+     *      예를 들어, 현재 로그인한 사용자가 작성한 포스트만 가져오려면,
+     *      토큰 sub 값에 입력돼있는 사용자의 포스트만 따로 필터링 할 수 있다.
+     *      특정 사용자의 토큰이 없다면 다른 사용자의 데이터를 접근 못한다.
+     * 5) 모든 토큰은 만료 기간이 있음, 만료기간이 지난다면 새로 토큰 발급 필요
+     *      그렇지 않으면 jwtService.verify()에서 인증이 통과 안된다.
+     *      그러니 access 토큰을 새로 발급받을 수 있는 /auth/token/access와
+     *      refresh 토큰을 새로 발급 받을 수 있는 /auth/token/refresh 가 필요하다.
+     * 6) 토큰이 만료되면, 각각의 토큰을 새로 발급 받을 수 있는 엔드포인트에 요청을 해서
+     *      새로운 토큰을 발급받고, 새로운 토큰을 사용해서 private route 에 접근한다.
+     */
+
+    /**
+     *  Header로부터 토큰을 받을 때
+     *      예) {authorization: 'Basic {toekn}'}
+     *      예) {authorization: 'Bearer {token}'}
+     */
+
+    extractTokenFromHeader(header: string, isBearer: boolean) {
+        const splitToken = header.split(' ');
+        const prefix = isBearer ? 'Bearer' : 'Basic';
+
+        if(splitToken.length !== 2 || splitToken[0]!== prefix){
+            throw new UnauthorizedException('잘못된 토큰입니다.');
+        }
+
+        const token = splitToken[1];
+
+        return token;
+    }
+    
+    decodeBasicToken(base64String: string) {
+        const decoded = Buffer.from(base64String, 'base64').toString('utf8');
+        const split = decoded.split(':');
+
+        if (split.length !== 2) {
+            throw new UnauthorizedException(`잘못된 유형의 토큰입니다.`);
+        }
+    }
+
+    verifyToken(token: string){
+        return this.jwtService.verify(token,{
+            secret: this.configService.get<string>(ENV_JWT_SECRET_KEY)
+        });
+    }
+
+    rotateToken(token: string, isRefreshToken: boolean){
+        const decoded = this.jwtService.verify(token, {
+            secret: this.configService.get<string>(ENV_JWT_SECRET_KEY)
+        })
+        
+        if(decoded.type !== 'refresh'){
+            throw new UnauthorizedException(`토큰 재발급은 리프레쉬 토큰으로만 가능합니다.`);
+        }
+
+        return this.signToken({
+            ...decoded,
+        }, isRefreshToken);
+    }
+
+
+
+
+    signToken(arg0: any, isRefreshToken: boolean) {
+        throw new Error('Method not implemented.');
+    }
+
+    
+
+    
+
+
+
+
+
+
+}
