6185e1f4211ef68e3738df224b84b95aeaba4cfd
Kyoung Yeob <kkyub1313@gmail.com>
2024-09-07 15:40:12 +0900

    nest g resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { GoalsModule } from './goals/goals.module';
+import { CommonModule } from './common/common.module';
+import { AuthModule } from './auth/auth.module';
+import { PostsModule } from './posts/posts.module';
 
 @Module({
-  imports: [],
+  imports: [UsersModule, GoalsModule, CommonModule, AuthModule, PostsModule],
   controllers: [AppController],
   providers: [AppService],
 })
