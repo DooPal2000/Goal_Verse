31c35c1565fea9dd582a82283c854fedca31f046
Kyoung Yeob <kkyub1313@gmail.com>
2024-09-07 18:09:33 +0900

    entity, dto 제작

---
+++ src/posts/entity/posts.entity.ts
@@ -0,0 +1,50 @@
+import { Transform } from "class-transformer";
+import { IsString } from "class-validator";
+import { join } from "path";
+import { POST_PUBLIC_IMAGE_PATH } from "src/common/const/path.const";
+import { BaseModel } from "src/common/entity/base.entity";
+import { ImageModel } from "src/common/entity/image.entity";
+import { stringValidationMessage } from "src/common/validation-message/string-validation.message";
+import { UsersModel } from "src/users/entity/users.entity";
+import { Column, CreateDateColumn, Entity, ManyToOne, OneToMany, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+import { CommentsModel } from "../comments/entity/comments.entity";
+
+@Entity()
+export class PostsModel extends BaseModel {
+
+    @ManyToOne(() => UsersModel, (user) => user.posts, {
+        nullable: false
+    })
+    author: UsersModel;
+
+
+    @Column()
+    @IsString({
+        message: stringValidationMessage,
+    })
+    title: string;
+
+    @Column()
+    @IsString({
+        message: stringValidationMessage,
+    })
+    content: string;
+
+    // @Column({
+    //     nullable: true,
+    // })
+    // @Transform(({value})=> value && `/${join(POST_PUBLIC_IMAGE_PATH, value)}`)
+    // image?: string;
+
+    @Column()
+    likeCount: number;
+
+    @Column()
+    commentCount: number;
+
+    @OneToMany(() => ImageModel, (image) => image.post)
+    images: ImageModel[];
+
+    @OneToMany(() => CommentsModel, (comment) => comment.post)
+    comments: CommentsModel[];
+}
---
+++ src/users/entity/users.entity.ts
@@ -0,0 +1,82 @@
+import { Column, CreateDateColumn, Entity, JoinTable, ManyToMany, ManyToOne, OneToMany, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+import { RolesEnum } from "../const/roles.const";
+import { PostsModel } from "src/posts/entity/posts.entity";
+import { BaseModel } from "src/common/entity/base.entity";
+import { IsEmail, IsString, Length, ValidationArguments } from "class-validator";
+import { lengthValidationMessage } from "src/common/validation-message/length-validation.message";
+import { stringValidationMessage } from "src/common/validation-message/string-validation.message";
+import { emailValidationMessage } from "src/common/validation-message/email-validation.message";
+import { Exclude } from "class-transformer";
+
+@Entity()
+// @Exclude() // 보안이 매우 중요한 객체라면, entity 자체를 exclude 할 수도 있다. 그 이후에 expose() 를 활용하는 방식이다.
+export class UsersModel extends BaseModel {
+
+    @Column({
+        length: 20,
+        unique: true,
+    })
+    @IsString({
+        message: stringValidationMessage,
+    })
+    @Length(1, 20, {
+        message: lengthValidationMessage,
+    })
+    nickname: string;
+
+    @Column({
+        unique: true,
+    })
+    @IsString({
+        message: stringValidationMessage,
+    })
+    @IsEmail({}, {
+        message: emailValidationMessage,
+    })
+    email: string;
+
+    @Column()
+    @IsString({
+        message: stringValidationMessage,
+    })
+    @Length(3, 8, {
+        message: lengthValidationMessage,
+    })
+    /**
+     * Exclude 옵션
+     * Request 
+     * 프엔 -> 백엔
+     * plain object (JSON) -> class instance(dto) 
+     * 
+     * Response
+     * 백엔 -> 프엔
+     * class instance(dto) -> plain object (JSON)
+     * 
+     * *toClassOnly -> class instance 로 변환될때만 제외
+     * *toPlainOnly -> plain object 로 변환될때만 제외
+     */
+    @Exclude({
+        toPlainOnly: true,
+    })
+    password: string;
+
+    @Column({
+        enum: Object.values(RolesEnum),
+        default: RolesEnum.USER,
+    })
+    role: RolesEnum;
+
+    @OneToMany(() => PostsModel, (post) => post.author)
+    posts: PostsModel[];
+
+    @ManyToMany(() => ChatsModel, (chat) => chat.users)
+    @JoinTable()
+    chats: ChatsModel[];
+
+    @OneToMany(() => MessagesModel, (msg) => msg.author)
+    messages: MessagesModel;
+
+    @OneToMany(() => CommentsModel, (comment) => comment.author)
+    postComments: CommentsModel[];
+
+}
