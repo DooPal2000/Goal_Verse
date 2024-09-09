667c0f48041fb6636f3548ba0a8128ded98e0911
Kyoung Yeob <kkyub1313@gmail.com>
2024-09-09 23:43:09 +0900

    chore(config): update TypeORM configuration and modify modules
    Updated TypeORMModule.forRoot settings in app.module and made adjustments to auth, common, and users modules.

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,10 +6,45 @@ import { GoalsModule } from './goals/goals.module';
 import { CommonModule } from './common/common.module';
 import { AuthModule } from './auth/auth.module';
 import { PostsModule } from './posts/posts.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ENV_DB_DATABASE_KEY, ENV_DB_HOST_KEY, ENV_DB_PASSWORD_KEY, ENV_DB_PORT_KEY } from './common/const/env-keys.const';
+import { PostsModel } from './posts/entity/posts.entity';
+import { UsersModel } from './users/entity/users.entity';
+import { ImageModel } from './common/entity/image.entity';
+import { CommentsModel } from './posts/comments/entity/comments.entity';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [UsersModule, GoalsModule, CommonModule, AuthModule, PostsModule],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+      isGlobal: true,
+    }),
+
+    UsersModule,
+    GoalsModule,
+    CommonModule,
+    AuthModule,
+    PostsModule,
+    TypeOrmModule.forRoot({
+      // 데이터베이스 타입
+      type: 'postgres',
+      host: process.env[ENV_DB_HOST_KEY],
+      port: parseInt(process.env[ENV_DB_PORT_KEY]),
+      username: process.env[ENV_DB_DATABASE_KEY],
+      password: process.env[ENV_DB_PASSWORD_KEY],
+      database: process.env[ENV_DB_DATABASE_KEY],
+      entities: [
+        PostsModel,
+        UsersModel,
+        ImageModel,
+        CommentsModel,
+      ],
+      synchronize: true,
+    }),
+
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
